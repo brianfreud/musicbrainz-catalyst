a8fdb7d9951b34fc3cdde728344e5f4e4140af0c
Robert Kaye <robert@rump.(none)>
2009-02-02 20:51:30 -0800

    Finish adding pagers to the pages. This branch is ready for a review!

--- lib/MusicBrainz/Server/Controller/News.pm
+++ lib/MusicBrainz/Server/Controller/News.pm
@@ -29,32 +29,40 @@ sub music_news : Local Arg(1)
     my $page  = $c->req->query_params->{page} || 1;
 
     $c->stash->{template} = "news/musicnews.tt";
+    my $pager = Data::Page->new;
+    $pager->entries_per_page(25);
+    $pager->current_page($page);
     if ($type eq 'upcoming_releases')
     {
-	my $pager = Data::Page->new;
-
 	($c->stash->{upcoming_releases}, $c->stash->{upcoming_releases_numitems}, $c->stash->{upcoming_releases_timestamp}) =
 	    $c->model('News')->upcoming_releases($pager->entries_per_page, ($page - 1) * $pager->entries_per_page);
 
 	$pager->total_entries($c->stash->{upcoming_releases_numitems});
-	$pager->entries_per_page(25);
-	$pager->current_page($page);
 	$c->stash->{pager} = $pager;
     } 
     elsif ($type eq 'recent_releases')
     {
 	($c->stash->{recent_releases}, $c->stash->{recent_releases_numitems}, $c->stash->{recent_releases_timestamp}) =
-	    $c->model('News')->recent_releases(10, 0);
+	    $c->model('News')->recent_releases($pager->entries_per_page, ($page - 1) * $pager->entries_per_page);
+
+	$pager->total_entries($c->stash->{recent_releases_numitems});
+	$c->stash->{pager} = $pager;
     } 
     elsif ($type eq 'recently_deceased')
     {
 	($c->stash->{recently_deceased}, $c->stash->{recently_deceased_numitems}, $c->stash->{recently_deceased_timestamp}) =
-	    $c->model('News')->recently_deceased(10, 0);
+	    $c->model('News')->recently_deceased($pager->entries_per_page, ($page - 1) * $pager->entries_per_page);
+
+	$pager->total_entries($c->stash->{recently_deceased_numitems});
+	$c->stash->{pager} = $pager;
     }
     elsif ($type eq 'recently_brokenup')
     {
 	($c->stash->{recently_brokenup}, $c->stash->{recently_brokenup_numitems}, $c->stash->{recently_brokenup_timestamp}) =
-	    $c->model('News')->recently_brokenup(10, 0);
+	    $c->model('News')->recently_brokenup($pager->entries_per_page, ($page - 1) * $pager->entries_per_page);
+
+	$pager->total_entries($c->stash->{recently_brokenup_numitems});
+	$c->stash->{pager} = $pager;
     }
     else
     {

3c3120b943aee7c03452a6f4065883f15998d0f7
Brian Schweitzer <brian@Silverberg.(none)>
2009-02-12 06:53:11 -0500

    Disable sorting on the editing links column

--- root/static/scripts/artistreleases.js
+++ root/static/scripts/artistreleases.js
@@ -19,3 +19,5 @@ $(document).ready(function() {
     });
 });
 
+
+/* Breaks on Live and Other release title at http://brianfreud.info:3001/artist/183d6ef6-e161-47ff-9085-063c8b897e97?show_all=1 */
--- root/static/scripts/release.js
+++ root/static/scripts/release.js
@@ -31,6 +31,7 @@ $(document).ready(function() {
     /* Make the table sortable. */
     $("thead").toggle();
     $(".release_tracks").tablesorter({
-        textExtraction: "complex"
+        textExtraction: "complex",
+        headers: { 4: { sorter: false}}
     });
 });

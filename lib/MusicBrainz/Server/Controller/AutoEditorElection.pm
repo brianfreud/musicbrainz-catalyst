fe840ded127b2f4f3f211ae5062893d0a6835ae0
Oliver <oliver.g.charles@googlemail.com>
2009-04-14 12:17:04 +0100

    Support proposing editors to become auto-editors

--- lib/MusicBrainz/Server/Controller/AutoEditorElection.pm
+++ lib/MusicBrainz/Server/Controller/AutoEditorElection.pm
@@ -3,11 +3,14 @@ package MusicBrainz::Server::Controller::AutoEditorElection;
 use strict;
 use warnings;
 
-use base 'Catalyst::Controller';
+use base 'MusicBrainz::Server::Controller';
+
+use Exception::Class;
+use MusicBrainz::Server::Form::AutoEditorElection::Propose;
 
 __PACKAGE__->config(namespace => 'elections');
 
-sub elections : Path('')
+sub elections : Path('') Form('AutoEditorElection::Propose')
 {
     my ($self, $c) = @_;
     $c->forward('/user/login');
@@ -18,6 +21,52 @@ sub elections : Path('')
     for my $el (@{ $c->stash->{elections}})
     {
         $el->candidate->Refresh;
+    }
+}
+
+sub propose : Local Form('AutoEditorElection::Propose')
+{
+    my ($self, $c) = @_;
+    
+    return unless $self->submit_and_validate($c);
+
+    my $form = $self->form;
+    my $editor = $c->model('User')->load({ username => $form->value('editor') });
+
+    my $election;
+    eval {
+        $election = $c->model('AutoEditorElection')->propose($editor, $c->user);
+    };
+
+    if (my $e = Exception::Class->caught('AlreadyAutoEditorException'))
+    {
+        $form->field('editor')->add_error('This editor is already an auto-editor.');
+    }
+    elsif (my $e = Exception::Class->caught('EditorIneligibleException'))
+    {
+        $form->field('editor')->add_error('This editor is ineligible to become an auto-editor.');
+    }
+    elsif (my $e = Exception::Class->caught('ElectionAlreadyExistsException'))
+    {
+        $c->response->redirect($c->uri_for('/elections', $e->election_id));
+    }
+    else
+    {
+        $c->stash->{election_id} = $election->id;
+        $c->stash->{candidate} = $editor;
+        $c->stash->{template} = 'elections/proposed.tt';
+    }
+}
+
+sub login : Private
+{
+    my ($self, $c) = @_;
+    $c->forward('/user/login');
+
+    if (!$c->user->is_auto_editor)
+    {
+        $c->stash->{template} = 'elections/no_privs.tt';
+        $c->detach;
     }
 }
 
